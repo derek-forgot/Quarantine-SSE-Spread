a037b79df3c85bc2991efd12618d83e27018203f
derek-forgot <153856317+derek-forgot@users.noreply.github.com>
2025-12-13 13:54:04 -0500

    MAJOR CHANGES

--- simulate_absir.m
+++ simulate_absir.m
@@ -51,15 +51,17 @@ function [Sh, Ih, Rh,Qh,Eh] = simulate_absir(Iv0, T, recovery_rate, quarantine_p
        v_recover = rand(dim, 1) <= recovery_rate;
 
        %Compute which people can go from infected to quarantined
-       v_q = rand(dim,1) <= quarantine_percent;
+       v_quarantine = rand(dim,1) <= quarantine_percent;
 
        %Compute which people can go susceptible to exposed
-       v_exposed = rand(dim,1)<= new_infect_count/dim;
+       v_exposed(1:new_infect_count,1) = 1;
+       v_exposed(new_infect_count+1:dim,1) = 0;
+       v_exposed = v_exposed(randperm(length(v_exposed)));
      
        % Update all the stuff (._.)
        En = E | (v_exposed & ~I & ~E & ~R & ~Q);
        In = I | (v_exposed_to_infected & E);
-       Qn = Q | (v_q & I);
+       Qn = Q | (v_quarantine & I);
        Rn = R | (v_recover & (I | Q));
        En = En & ~In;
        Qn = Qn & ~Rn;
